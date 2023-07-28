6bb5311fc48f4def40c6864a146784594777e3f6
Goddchen <goddchen@gmail.com>
2023-07-28 22:16:52 +0200

    feat: resolve PR comments

--- README.md
+++ README.md
@@ -316,6 +316,54 @@ class FlutterDeckBulletListDemoSlide extends FlutterDeckSplitSlide {
 }
 ```
 
+### FlutterDeckCodeHighlight
+
+Provides a widget that gives you customizable syntax highlighting for many languages.
+
+```dart
+import 'package:flutter/widgets.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+
+class CodeHighlightSlide extends FlutterDeckBlankSlide {
+  const CodeHighlightSlide({
+    super.key,
+  }) : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            route: '/code-highlight',
+            header: FlutterDeckHeaderConfiguration(
+              title: 'Code Highlighting',
+            ),
+          ),
+        );
+
+  @override
+  Widget body(BuildContext context) => const Center(
+        child: FlutterDeckCodeHighlight(
+          code: '''
+import 'package:flutter_deck/flutter_deck.dart';
+
+class TitleSlide extends FlutterDeckTitleSlide {
+  const TitleSlide({super.key})
+      : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            route: '/intro',
+            footer: FlutterDeckFooterConfiguration(showFooter: false),
+          ),
+        );
+
+  @override
+  String get title => 'Welcome to flutter_deck example! 🚀';
+
+  @override
+  String? get subtitle => 'Use left and right arrow keys to navigate.';
+}''',
+          fileName: 'title_slide.dart',
+          language: 'dart',
+        ),
+      );
+}
+```
+
 ## Accessing slide deck state from the code
 
 By using the `FlutterDeck` extensions, you can access the slide deck state and its methods from anywhere in the app:
--- lib/src/widgets/flutter_deck_code_highlight.dart
+++ lib/src/widgets/flutter_deck_code_highlight.dart
@@ -6,41 +6,76 @@ import 'package:flutter_highlight/themes/vs2015.dart';
 /// This widget provides syntax highlighting for many languages.
 class FlutterDeckCodeHighlight extends StatelessWidget {
   /// Creates a syntax highlighting widget for displaying code.
+  ///
+  /// Will use [vs2015Theme] for dark theme and [defaultTheme] for light theme.
+  ///
+  /// For a list of all available [language] values, see:
+  /// https://github.com/git-touch/highlight.dart/tree/master/highlight/lib/languages
+  ///
+  /// For a list of all available [darkTextStyles] and [lightTextStyles]
+  /// values, see:
+  /// https://github.com/git-touch/highlight.dart/tree/master/flutter_highlight/lib/themes
+  ///
+  /// [textStyle] will be merged with all theme text styles.
+  ///
+  /// Use [darkTextStyles] and [lightTextStyles] to provide or override the
+  /// whole theme.
   const FlutterDeckCodeHighlight({
     required String code,
     super.key,
+    Map<String, TextStyle>? darkTextStyles,
     String? fileName,
     String language = 'dart',
+    Map<String, TextStyle>? lightTextStyles,
+    TextStyle? textStyle,
   })  : _code = code,
+        _darkTextStyles = darkTextStyles,
         _fileName = fileName,
-        _language = language;
+        _language = language,
+        _lightTextStyles = lightTextStyles,
+        _textStyle = textStyle;
+
   final String _code;
+  final Map<String, TextStyle>? _darkTextStyles;
   final String? _fileName;
   final String _language;
+  final Map<String, TextStyle>? _lightTextStyles;
+  final TextStyle? _textStyle;
 
   @override
-  Widget build(BuildContext context) => DecoratedBox(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.background,
-          borderRadius: const BorderRadius.all(Radius.circular(16)),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (_fileName != null) Text(_fileName!),
-              HighlightView(
-                _code,
-                language: _language,
-                padding: const EdgeInsets.all(16),
-                theme: Theme.of(context).brightness == Brightness.dark
-                    ? vs2015Theme
-                    : defaultTheme,
-              ),
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+        borderRadius: const BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (_fileName != null) ...[
+              Text(_fileName!),
+              const SizedBox(height: 4),
             ],
-          ),
+            HighlightView(
+              _code,
+              language: _language,
+              padding: const EdgeInsets.all(16),
+              theme: (Theme.of(context).brightness == Brightness.dark
+                      ? _darkTextStyles ?? vs2015Theme
+                      : _lightTextStyles ?? defaultTheme)
+                  .map(
+                (key, value) => MapEntry(
+                  key,
+                  value.merge(_textStyle),
+                ),
+              ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
