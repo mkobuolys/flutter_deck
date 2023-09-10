437a1b625308678ac38db1627532dda435bcc614
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2023-09-11 00:21:33 +0300

    docs: add theming slide

--- example/lib/slides/theming_slide.dart
+++ example/lib/slides/theming_slide.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_deck/flutter_deck.dart';
 
 class ThemingSlide extends FlutterDeckSlideWidget {
@@ -12,12 +12,38 @@ class ThemingSlide extends FlutterDeckSlideWidget {
 
   @override
   FlutterDeckSlide build(BuildContext context) {
-    return FlutterDeckSlide.blank(
-      builder: (context) => Center(
+    return FlutterDeckSlide.split(
+      theme: FlutterDeckTheme.of(context).copyWith(
+        splitSlideTheme: const FlutterDeckSplitSlideThemeData(
+          leftBackgroundColor: Colors.blue,
+          leftColor: Colors.yellow,
+          rightBackgroundColor: Colors.yellow,
+          rightColor: Colors.blue,
+        ),
+      ),
+      leftBuilder: (context) => Center(
         child: Text(
-          'Theming',
-          style: FlutterDeckTheme.of(context).textTheme.bodyMedium,
-          textAlign: TextAlign.center,
+          'You can set the global theme for the whole deck by specifying '
+          'lightTheme and darkTheme properties in the FlutterDeckApp widget.',
+          style: FlutterDeckTheme.of(context)
+              .textTheme
+              .bodyMedium
+              .copyWith(fontWeight: FontWeight.w500),
+          textAlign: TextAlign.justify,
+        ),
+      ),
+      rightBuilder: (context) => Center(
+        child: Text(
+          'You can also override the theme for a single slide by specifying '
+          'the theme property in the FlutterDeckSlide widget. It will be '
+          'merged with the global theme (only the properties you specify will '
+          'be overridden).\n\nJust like in this slide. Not UX friendly, but '
+          'you get the idea.',
+          style: FlutterDeckTheme.of(context)
+              .textTheme
+              .bodyMedium
+              .copyWith(fontWeight: FontWeight.w500),
+          textAlign: TextAlign.justify,
         ),
       ),
     );
