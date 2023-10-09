55d26d50868848984427b39f97df1114efd6420f
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2023-10-10 02:03:34 +0300

    feat: initial commit for the flutter_deck_slide brick

---
+++ bricks/flutter_deck_slide/__brick__/{{name.snakeCase()}}_slide.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+
+class {{name.pascalCase()}}Slide extends FlutterDeckSlideWidget {
+  const {{name.pascalCase()}}Slide()
+      : super(
+          configuration: const FlutterDeckSlideConfiguration(
+            route: '/{{name.paramCase()}}',
+          ),
+        );
+
+  @override
+  FlutterDeckSlide build(BuildContext context) {
+    return {{#use_blank}}{{>blank}}{{/use_blank}}{{#use_custom}}{{>custom}}{{/use_custom}}{{#use_big_fact}}{{>big-fact}}{{/use_big_fact}}{{#use_image}}{{>image}}{{/use_image}}{{#use_split}}{{>split}}{{/use_split}}{{#use_template}}{{>template}}{{/use_template}}{{#use_title}}{{>title}}{{/use_title}}
+  }
+}
