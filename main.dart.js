99d45a3036634c285cbee02f51310391d2c075b5
mkobuolys <mkobuolys@users.noreply.github.com>
2023-12-23 18:49:07 +0000

    deploy: 253db1be493ec7e3d47f7e23b3a7267571fe3b1e

--- main.dart.js
+++ main.dart.js
@@ -24031,7 +24031,7 @@ $1(a){var s=$.c8
 s=(s==null?$.c8=A.eP(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/54a7145303f0dd9d0f93424a2e124eb4abef5091/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/3f3e560236539b7e2702f5ac790b2a4691b32d49/":s)+a},
 $S:103}
 A.ahc.prototype={
 $1(a){this.a.remove()
