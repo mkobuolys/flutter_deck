650c6ccb2e6ad92908d3a1215fc4c45389b894f2
Mangirdas Kazlauskas <mangirdas@kazlauskas.dev>
2023-12-28 16:38:41 +0200

    Release v0.10.0

--- lib/src/controls/controls.dart
+++ lib/src/controls/controls.dart
@@ -3,3 +3,4 @@ export 'flutter_deck_controls.dart';
 export 'flutter_deck_controls_configuration.dart';
 export 'flutter_deck_controls_listener.dart';
 export 'flutter_deck_controls_notifier.dart';
+export 'fullscreen/fullscreen.dart';
