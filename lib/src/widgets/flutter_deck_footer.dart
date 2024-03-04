eab84d65dc4c76e08ddd340467e305c6e7244aac
chooyan-eng <chooyan.engineer@gmail.com>
2024-03-04 14:56:50 +0900

    missing dart:ui import

--- lib/src/widgets/flutter_deck_footer.dart
+++ lib/src/widgets/flutter_deck_footer.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_deck/src/configuration/configuration.dart';
 import 'package:flutter_deck/src/flutter_deck.dart';
