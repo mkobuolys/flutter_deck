ad482f5e8d6204796f87f079aa553c717a8f0d4f
mkobuolys <mkobuolys@users.noreply.github.com>
2024-03-17 22:19:18 +0000

    deploy: 59512f0480d066a22e30f46369defc355c32465c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "99126c3752dd48e1d5ae469757d12306",
-"/": "99126c3752dd48e1d5ae469757d12306",
+const RESOURCES = {"index.html": "1fd145a096b0b7135a8b39ced69b61df",
+"/": "1fd145a096b0b7135a8b39ced69b61df",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
