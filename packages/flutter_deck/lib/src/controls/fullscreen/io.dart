98f4f3da27a9a286bbb7d25c714a277e41040256
Tomek Polański <tomek.polanski@klar.mx>
2024-08-17 15:22:34 +0200

    Update io.dart

--- packages/flutter_deck/lib/src/controls/fullscreen/io.dart
+++ packages/flutter_deck/lib/src/controls/fullscreen/io.dart
@@ -5,19 +5,35 @@ import 'package:window_manager/window_manager.dart';
 
 /// The dart:io implementation of [FlutterDeckFullscreenManagerBase].
 class FlutterDeckFullscreenManager implements FlutterDeckFullscreenManagerBase {
+  bool _initialized = false;
+
+  Future<WindowManager> get _instance async {
+    if (!_initialized) {
+      await windowManager.ensureInitialized();
+      _initialized = true;
+    }
+    return WindowManager.instance;
+  }
+
   @override
   bool canFullscreen() =>
       Platform.isLinux || Platform.isWindows || Platform.isMacOS;
 
   @override
-  Future<void> enterFullscreen() => WindowManager.instance.setFullScreen(true);
-
-  @override
-  Future<bool> isInFullscreen() async {
-    await windowManager.ensureInitialized();
-    return WindowManager.instance.isFullScreen();
+  Future<void> enterFullscreen() async {
+    final windowManager = await _instance;
+    return windowManager.setFullScreen(true);
   }
 
   @override
-  Future<void> leaveFullscreen() => WindowManager.instance.setFullScreen(false);
+  Future<bool> isInFullscreen() async {
+    final windowManager = await _instance;
+    return windowManager.isFullScreen();
+  }
+
+  @override
+  Future<void> leaveFullscreen() async {
+    final windowManager = await _instance;
+    return windowManager.setFullScreen(false);
+  }
 }
