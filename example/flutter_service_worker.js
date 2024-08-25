7a51f70715f5808d065453e85fc449be83284a94
mkobuolys <mkobuolys@users.noreply.github.com>
2024-08-25 22:38:33 +0000

    deploy: 2007e9177ec86d9da81e5869ece76e8b5eb6e350

--- example/flutter_service_worker.js
+++ example/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "main.dart.js": "6c1b4ab66c4f2915650c75acee3af60f",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "38da80b51434c8f69ad02cfde1a0a046",
-"flutter_bootstrap.js": "e60bc76690e874de52a6c242b1b8c0aa",
+"flutter_bootstrap.js": "80b7a894e85d97776910eb0aadc10e18",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
