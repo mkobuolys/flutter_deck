45cb9dcd2e5475e264d137cee6f2e21f2e947822
Mangirdas Kazlauskas <mangirdas@billo.app>
2024-12-30 02:14:21 +0200

    chore: update platform-related files for the example project

--- packages/flutter_deck/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/flutter_deck/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import screen_retriever
+import screen_retriever_macos
 import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
