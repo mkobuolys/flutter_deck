f714a94dba72c4f02e7bf374c6f913db0c9720cf
Mangirdas Kazlauskas <mangirdas@billo.app>
2026-02-16 12:11:51 +0200

    docs: use FlutterDeckPdfExportPlugin in the example

--- packages/flutter_deck/example/lib/main.dart
+++ packages/flutter_deck/example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_deck/flutter_deck.dart';
 import 'package:flutter_deck_example/l10n/l10n.dart';
 import 'package:flutter_deck_example/slides/slides.dart';
+import 'package:flutter_deck_pdf_export/flutter_deck_pdf_export.dart';
 import 'package:flutter_deck_pptx_export/flutter_deck_pptx_export.dart';
 import 'package:flutter_deck_web_client/flutter_deck_web_client.dart';
 
@@ -70,7 +71,7 @@ class FlutterDeckExample extends StatelessWidget {
         ),
       ),
       // Add custom functionality using plugins.
-      plugins: [FlutterDeckAutoplayPlugin(), FlutterDeckPptxExportPlugin()],
+      plugins: [FlutterDeckAutoplayPlugin(), FlutterDeckPptxExportPlugin(), FlutterDeckPdfExportPlugin()],
       // Presentation is build automatically from the list of slides.
       slides: [
         const TitleSlide(),
